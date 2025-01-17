9782d9f7b0aff37694b278824174f8f3d0b0a91f
SyedShayanArshad <syedshayan4study@gmail.com>
2025-01-17 20:21:13 +0500

    add mongodb

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import connectDB from './db/index.js';
+import dotenv from 'dotenv';
+
+
+dotenv.config({
+    path: "./env"
+});
+connectDB();
+
+
+
+
+
+
+
+
+/*
+import express from 'express';
+const app = express();
+(async ()=>{
+    try {
+        app.on('error',(error)=>{
+            console.error('Error: ', error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.log("Error: ",error);
+    }
+})()
+*/
